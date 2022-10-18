2c5c6177bdd9cb514ebfb2f912b06995ee69fe3a
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-10-18 08:46:29 -0700

    Done

--- task0.c
+++ task0.c
@@ -7,11 +7,21 @@
  */
 int _printf(const char *format, ...)
 {
-	int pc;
+	int printed_chars;
 	conver_t f_list[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
 	};
 	va_list arg_list;
 
@@ -22,5 +32,5 @@ int _printf(const char *format, ...)
 	/*Calling parser function*/
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
-	return (pc);
+	return (printed_chars);
 }
